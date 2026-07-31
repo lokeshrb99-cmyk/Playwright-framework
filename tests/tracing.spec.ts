8b717d56f83346558408c666e6706e4392655eec
Lokesh <lokeshrb99@gmail.com>
2026-07-31 12:48:50 +0530

    change has been done in the tracing

--- tests/tracing.spec.ts
+++ tests/tracing.spec.ts
@@ -1,20 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page, context }) => {
+test('test', async ({ page,context }) => {
+
+  //await context.tracing.start({screenshots:true, snapshots:true});
+
   await context.tracing.start({
-    screenshots: true,
-    snapshots: true,
-  });
+  screenshots: true,
+  snapshots: true,
+});
 
   await page.goto('https://demoblaze.com/');
   await expect(page.getByRole('link', { name: 'PRODUCT STORE' })).toBeVisible();
-
+  await expect(page.getByRole('link', { name: 'Log in' })).toBeVisible();
   await page.getByRole('link', { name: 'Log in' }).click();
+  await page.locator('#loginusername').click();
   await page.locator('#loginusername').fill('pavanol');
+  await page.locator('#loginpassword').click();
   await page.locator('#loginpassword').fill('test@123');
   await page.getByRole('button', { name: 'Log in' }).click();
-
-  await context.tracing.stop({
-    path: 'trace.zip',
-  });
+ // await expect(page.locator('#nameofuser')).toContainText('Welcome pavanol');
+ // await expect(page.getByRole('link', { name: 'Log out' })).toBeVisible();
+ // await page.getByRole('link', { name: 'Log out' }).click();
+  await context.tracing.stop({path:'trace.zip'});
+  
 });
